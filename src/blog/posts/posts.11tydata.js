2491e55ab7c8dbeb7f6acd1357b7caadcbe11cab
YourLocalGrubdog <grubdog@proton.me>
2024-12-31 18:10:50 -0500

    backend updates for future rss update

--- src/blog/posts/posts.11tydata.js
+++ src/blog/posts/posts.11tydata.js
@@ -1,8 +1,4 @@
-const {DateTime} = require("luxon")
 module.exports = {
     layout: "blog-base.html",
     tags: ["posts"],
-    eleventyComputed: {
-        dateString: ({page}) => DateTime.fromJSDate(page.date, {zone: 'utc'}).toLocaleString(DateTime.DATE_FULL)
-    }
 }
